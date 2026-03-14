d0405faa8d3b443243af5080359ddcac69b22c88
Artem <stuzukartem12@gmail.com>
2026-03-14 22:35:04 +0200

    TS

--- src/task-8.ts
+++ src/task-8.ts
@@ -6,8 +6,8 @@ interface Post {
   body: string;
 }
 
-async function fetchPosts(): Promise<Post> {
-  const response = await axios.get<Post>(
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
     "https://jsonplaceholder.typicode.com/posts",
   );
   return response.data;
